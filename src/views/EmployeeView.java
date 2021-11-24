95754e6e05ea373a6eb18be4a07961e40ae7083d
maksym <osypchukmaxim.m8@gmail.com>
2021-11-24 20:43:27 +0200

    Add EmployeeView to view with basic vars and imports

---
+++ src/views/EmployeeView.java
@@ -0,0 +1,16 @@
+package views;
+
+import models.Employee;
+import utils.Validator;
+
+import java.util.Scanner;
+
+public class EmployeeView {
+
+    Employee model;
+    String title;
+    double hourlyRate;
+    double totalHours;
+    Scanner scanner;
+
+}
