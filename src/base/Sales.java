27aceabce68fcd60eb1db5388a9fb46cb7f24179
maksym <osypchukmaxim.m8@gmail.com>
2021-11-24 19:47:05 +0200

    Fix Sales interface with correct parameters

--- src/base/Sales.java
+++ src/base/Sales.java
@@ -2,7 +2,7 @@ package base;
 
 public interface Sales {
 
-    double calculateSales(int quota, double price);
+    double calculateSales(int unitsAmount, double price);
 
-    double calculateSalesTaxes(double payment, double discount);
+    double calculateSalesTaxes(double salesAmount, double taxRate);
 }
